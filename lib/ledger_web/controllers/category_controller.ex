16d0c1d492bcf307fddaeff491f13a3f4c3a8272
Denver Smith <denverpsmith@gmail.com>
2018-10-21 17:38:51 -0600

    Finish budget association with transactions

--- lib/ledger_web/controllers/category_controller.ex
+++ lib/ledger_web/controllers/category_controller.ex
@@ -24,4 +24,24 @@ defmodule LedgerWeb.CategoryController do
         render(conn, "new.html", changeset: changeset)
     end
   end
+
+  def edit(conn, %{"id" => id}) do
+    category = Budgets.get_category!(id)
+    changeset = Budgets.change_category(category)
+
+    render(conn, "edit.html", category: category, changeset: changeset)
+  end
+
+  def update(conn, %{"id" => id, "category" => category_params}) do
+    category = Budgets.get_category!(id)
+
+    case Budgets.update_category(category, category_params) do
+      {:ok, _account} ->
+        conn
+        |> put_flash(:info, "Budget updated successfully.")
+        |> redirect(to: category_path(conn, :index))
+      {:error, %Ecto.Changeset{} = changeset} ->
+        render(conn, "edit.html", category: category, changeset: changeset)
+    end
+  end
 end
