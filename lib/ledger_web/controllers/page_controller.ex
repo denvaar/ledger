dc8879f9a2c2aa262bc39398109251cf0131b297
Denver Smith <denverpsmith@gmail.com>
2018-10-21 21:22:58 -0600

    Add aggregations and report section

--- lib/ledger_web/controllers/page_controller.ex
+++ lib/ledger_web/controllers/page_controller.ex
@@ -6,7 +6,8 @@ defmodule LedgerWeb.PageController do
     savings_amount = Ledger.Budgets.monthly_savings()
     expense_amount = Ledger.Budgets.monthly_expenses()
     total_balance = Ledger.Budgets.total_balance()
+    amounts_by_budget = Ledger.Budgets.amounts_by_budget()
 
-    render(conn, "index.html", total_balance: total_balance, current_month: current_month, savings_amount: savings_amount, expense_amount: expense_amount)
+    render(conn, "index.html", total_balance: total_balance, current_month: current_month, savings_amount: savings_amount, expense_amount: expense_amount, amounts_by_budget: amounts_by_budget)
   end
 end
